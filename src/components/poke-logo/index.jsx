9cde6e05bd426dc2120c84408d7a7f5317e60653
Joab <joobascanal@gmail.com>
2024-12-21 18:49:45 -0300

    mudando diretório do poke-logo

--- src/components/poke-logo/index.jsx
+++ src/components/poke-logo/index.jsx
@@ -3,7 +3,7 @@ import { Logo } from "./css"
 const PokeLogo = () => {
 
     return (
-        <Logo src="./public/imgs/pokemon-logo.png"></Logo>
+        <Logo src="../../imgs/pokemon-logo.png"></Logo>
     )
 }
 
