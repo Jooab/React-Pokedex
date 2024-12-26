878218b087bc896c7256018dfa7b2ac72eb7c040
Joab <joobascanal@gmail.com>
2024-12-26 16:58:51 -0300

    adicionado o hover na logo

--- src/components/poke-logo/css.jsx
+++ src/components/poke-logo/css.jsx
@@ -5,6 +5,12 @@ export const Logo = styled.img`
     height: 180px;
     margin-left: 50px;
 
+    &:hover {
+        cursor: pointer;
+        transform: scale(1.05);
+        transition: 0.3s;
+    }
+
     @media (max-width: 768px) {
         width: 250px;
         height: 120px;
